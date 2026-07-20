5254d200d9b550bc19518252d5e254627d0c5afb
liudmylatkachenkooffcl <liudmyla.tkachenko.offcl@gmail.com>
2026-07-20 23:53:03 +0200

    Create roots.py

---
+++ roots.py
@@ -0,0 +1,19 @@
+#loremipsum XDD
+import db_utils
+class user:
+    def __init__(self):
+        self.username = ""
+        self.user_id = ""
+        self.username = ""
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
+        # self.profile_pic = '?'
+        self.bio = ""
+        self.is_active = bool
+        # self.created = '?'
+        self.is_moderator = bool
+
+    def pack_in_json(): #welp, I'm gonna start with the hardest one XDD
+        pass
