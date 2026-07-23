e160a879beba5b7eae4698493d297a69d5214778
liudmylatkachenkooffcl <liudmyla.tkachenko.offcl@gmail.com>
2026-07-23 22:15:25 +0200

    Update roots.py

--- roots.py
+++ roots.py
@@ -1,6 +1,10 @@
 #loremipsum XDD
 import db_utils
 import json
+import datetime
+from flask import Flask, request
+import os
+import uuid
 class User:
     def __init__(self):
         self.username = None
@@ -9,15 +13,37 @@ class User:
         self.password = ""
         self.first_name = ""
         self.last_name = ""
-        self.profile_pic = None
+        self.profile_pic = "" #is in link, we're taking it from folder from server
         self.bio = ""
         self.is_active = True
-        self.created = None
+        self.created = None #datetime format
         self.is_moderator = False
 
-    def pack_in_json(self): #welp, I'm gonna start with the hardest one XDD
-        json_dict = {
-            "user_id":self.user_id
+    def dict_not_secure(self): #Contains no password and email. Here is only publically avaiable data. Maybe I should remove is_actve
+        dict_not_secure = {
+            "user_id":self.user_id,
+            "username":self.username,
+            "first_name":self.first_name,
+            "last_name":self.last_name,
+            "profile_pic":self.profile_pic,
+            "bio":self.bio,
+            "is_active":self.is_active,
+            "created":self.created,
+            "is_moderator":self.is_moderator
+        }
+
+    def pack_in_json(pack_dict, self): #just dict to json, maybe used for every dict
+        return json.dumps(pack_dict)
+
+    def import_from_db_public(self, user_id): #we get everything besides email and password.
+        imported_data = db_utils.recall_user(user_id)
+        self.username = imported_data[1]
+        self.first_name = imported_data[2]
+        self.last_name = imported_data[3]
+        self.profile_pic = imported_data[4]
+        self.bio = imported_data[5]
+        self.created = imported_data[6]
+        #add maybe some message to console with "[SUCESS]User [user_id] data was recalled sucessfully"
 
 
 
